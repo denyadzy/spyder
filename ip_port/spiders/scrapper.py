8b8dee0de3e86902c502d8b6b2f97d74f8ad3a6b
denya-rog <denya-rog@mail.ru>
2017-12-12 02:15:56 +0500

    more conditions

--- ip_port/spiders/scrapper.py
+++ ip_port/spiders/scrapper.py
@@ -33,30 +33,32 @@ class PortSpider(scrapy.Spider):
         #using property that a^b^b=a we can just found variable, and save its nubmer befor xor
         if dependences[-1]==";":
             dependences = dependences[:-1]
+            
         dependences = dependences.split(";")
 
         # found all xor statemnts
         keys = [x for x in dependences if "^"  in x]
-        values = [x for x in dependences if "^"  not in x]
+        values = [x for x in dependences if x  not in keys]
         
         dic_params = {} # dic of params 
         for i in values:
-            # spliting string into encrypted var, number and decrypter var
-            devided = i.split("=")
-            dic_params[devided[0]] = int(devided[1])
+            # spliting string into encrypted var and number 
+            splited = i.split("=")
+            dic_params[splited[0]] = int(splited[1])
             
-        dic_numbers = {} # dic of encrypted numbers
-        dic_vars ={}
+        dic_numbers = {}    # dict of XOR number and var
+        dic_vars ={}        # dict of XOR of two vars
+        
         for i in keys:
             # spliting string into encrypted var, number and decrypter var
-            devided = i.split("=")
+            splited = i.split("=")
             #while in encrypting uses only one operand ^ we can devideinto value and parametr: 
             try:
-                encrypted = devided[0]
-                num_cryp = devided[1].split("^")
+                encrypted = splited[0]
+                num_cryp = splited[1].split("^")
             except:
-                encrypted = devided[1]
-                num_cryp = devided[0].split("^")
+                encrypted = splited[1]
+                num_cryp = splited[0].split("^")
             
             try:
                 
@@ -69,7 +71,7 @@ class PortSpider(scrapy.Spider):
                      
                 except:
                     
-                    try:# cathing, if XOR to two variables
+                    try:# cathing, if XOR to two variables from list
                         dic_vars[encrypted] = dic_params[num_cryp[0]]^dic_params[num_cryp[1]]
                         
                     except:
@@ -86,6 +88,7 @@ class PortSpider(scrapy.Spider):
            
             port = ""
             for item in variables:
+                # findind value of XOR of value and variable
                 if item[0] in dic_numbers:
                     port += dic_numbers[item[0]]
                     
@@ -93,13 +96,14 @@ class PortSpider(scrapy.Spider):
                     port += dic_numbers[item[1]]
                     
                 else:
+                    # catching  variable what equals to XOR of two variables
                     try:
                         port += str(dic_vars[item[0]]^dic_params[item[1]])
                     except:
                         
                         try:
                             port += str(dic_vars[item[1]]^dic_params[item[0]])
-                        
+                        # can't find any match
                         except:
                             print("some undefinite encryped item", item)
                 
